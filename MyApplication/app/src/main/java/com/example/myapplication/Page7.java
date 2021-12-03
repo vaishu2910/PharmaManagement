230e0ae430662700d88a4421822fbc1b6667d59e
vaishu2910 <80452670+vaishu2910@users.noreply.github.com>
2021-12-03 11:38:55 +0530

    Add files via upload

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Page7.java
@@ -0,0 +1,43 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class Page7 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page7);
+
+        Spinner sp = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.numbers, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/page2.java
@@ -0,0 +1,56 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class page2 extends AppCompatActivity {
+    public Button btn,btn2,btn3,btn4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2);
+
+        btn = (Button) findViewById(R.id.doctor);
+        btn2 = (Button) findViewById(R.id.product);
+        btn3 = (Button) findViewById(R.id.add_doctor);
+        btn4 = (Button) findViewById(R.id.add_product);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(page2.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2= new Intent(page2.this,MainActivity.class);    //here add class name of that page which we expected to open after clicking on this button!!
+                startActivity(intent2);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3= new Intent(page2.this,MainActivity.class);    //here add class name of that page which we expected to open after clicking on this button!!
+                startActivity(intent3);
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent4= new Intent(page2.this,MainActivity.class);    //here add class name of that page which we expected to open after clicking on this button!!
+                startActivity(intent4);
+            }
+        });
+    }
+}
